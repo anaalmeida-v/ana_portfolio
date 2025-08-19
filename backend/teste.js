7e61905a5d553219e15721ef9412369621edef6a
Ana Carolina de Almeida <aninhaalmeidav@gmail.com>
2025-08-19 18:11:29 -0300

    feat: adiciona arquivo no .gitignore + instala pacotes necessários + inicia configuração do BD no projeto

---
+++ backend/teste.js
@@ -0,0 +1,11 @@
+require('.env').config();
+
+// Exemplo de uso
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+console.log(connection)
