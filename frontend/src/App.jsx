be0f994c9b1cff380e3d4ac4c767b8e59f8d4acf
Ana Almeida <aninhaalmeidav@gmail.com>
2026-06-26 16:32:44 -0300

    feat(front): renderiza a seção home sobre o fundo 3d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { FundoInterativo } from './components/FundoInterativo';
 import { Header } from './components/Header';
+import { Home } from './components/Home';
 
 function App() {
 
   return (
     <>
       <Header />
+      <Home />
       <FundoInterativo />
     </>
   )
