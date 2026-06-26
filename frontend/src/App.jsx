cb9038db6d0bc66120b9b4267d7467231e3ca0d4
Ana Almeida <aninhaalmeidav@gmail.com>
2026-06-26 13:42:25 -0300

    feat(front): renderiza o componente header na tela principal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { FundoInterativo } from './components/FundoInterativo';
+import { Header } from './components/Header';
 
 function App() {
 
   return (
     <>
+      <Header />
       <FundoInterativo />
     </>
   )
