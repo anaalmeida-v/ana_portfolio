7e79d00f04f39e73087350a91a8d80f64c6c5c03
Ana Almeida <aninhaalmeidav@gmail.com>
2026-08-14 17:39:33 -0300

    refactor(front): corrige info rodapé

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -292,7 +292,7 @@ export const Home = () => {
       </section>
 
       <footer className="footer">
-        <p>ana almeida - dev front-end / web developes | 2026</p>
+        <p>ana almeida - dev front-end / web developer | 2026</p>
       </footer>
     </main>
   );
